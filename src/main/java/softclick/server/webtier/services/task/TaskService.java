9e8a6d933c329018bbbb1a02ab1dcac4e571c1c9
othmane <othmane.hanyf@protonmail.com>
2022-12-01 18:22:06 +0100

    setting up spring security and adding UserController

--- src/main/java/softclick/server/webtier/services/task/TaskService.java
+++ src/main/java/softclick/server/webtier/services/task/TaskService.java
@@ -1,12 +1,9 @@
 package softclick.server.webtier.services.task;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.jpa.repository.JpaRepository;
 import softclick.server.data.entities.Task;
 import softclick.server.data.repositories.TaskRepository;
-import softclick.server.data.repositories.UserRepository;
 import softclick.server.webtier.services.BaseService;
-import softclick.server.webtier.services.auth.AuthService;
 
 public class TaskService extends BaseService<Task, Long> implements ITaskService {
 
