1ce01b4fa9170aa05766b60c1a8cde47e86ddbe8
wafaeabo <wafae.abouzbiba@uit.ac.ma>
2022-12-08 12:52:04 +0000

    client tests

---
+++ src/test/java/softclick/server/webtier/services/client/ClientServiceTest.java
@@ -0,0 +1,54 @@
+package softclick.server.webtier.services.client;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import softclick.server.data.entities.*;
+import softclick.server.data.repositories.ClientRepository;
+import softclick.server.data.repositories.TaskRepository;
+import softclick.server.webtier.services.task.ITaskService;
+import softclick.server.webtier.services.task.TaskService;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientServiceTest {
+
+    @Mock
+    private ClientRepository clientRepository;
+
+    private IClientService serviceUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        serviceUnderTest = new ClientService(clientRepository);
+    }
+
+    @Test
+    void itShouldVerifyClientAdded() {
+        // given
+
+
+        Client client = new Client("abouzbiba","wafae","wafae.abouzbiba@gmail.com","06 37 94","soft","temara","maroc");
+
+        // when
+        serviceUnderTest.saveEntity(client);
+        // then
+        ArgumentCaptor<Client> clientArgumentCaptor = ArgumentCaptor.forClass(Client.class);
+        verify(clientRepository).save(clientArgumentCaptor.capture());
+        Client capturedClient = clientArgumentCaptor.getValue();
+        assertThat(capturedClient).isEqualTo(client);
+    }
+
+
+
+}
