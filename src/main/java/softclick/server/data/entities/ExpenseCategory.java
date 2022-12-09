cef1702e924485b5e26be6f79d02dc380da7eb53
othmane <othmane.hanyf@protonmail.com>
2022-12-09 16:41:56 +0100

    fixing some issues in ExpenseCategory

--- src/main/java/softclick/server/data/entities/ExpenseCategory.java
+++ src/main/java/softclick/server/data/entities/ExpenseCategory.java
@@ -9,23 +9,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
-import java.util.Comparator;
 
 @Entity
 @NoArgsConstructor
 @Data
 @Proxy(lazy=false)
-public class ExpenseCategory implements Serializable, Comparable<ExpenseCategory> {
+public class ExpenseCategory implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String category;
     public ExpenseCategory(String category){
         this.category=category;
-    }
-
-    @Override
-    public int compareTo(ExpenseCategory expenseCategory) {
-        return Comparator.comparing(ExpenseCategory::getCategory).compare(this, expenseCategory);
     }
 }
