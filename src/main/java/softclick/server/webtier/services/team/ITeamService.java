794d5778d54c04ef3c134373ce25e6d2771b8705
manal2000nwbd <88880532+manal2000nwbd@users.noreply.github.com>
2022-12-10 12:34:37 +0100

    commit_team_api

--- src/main/java/softclick/server/webtier/services/team/ITeamService.java
+++ src/main/java/softclick/server/webtier/services/team/ITeamService.java
@@ -1,7 +1,10 @@
 package softclick.server.webtier.services.team;
 
+import softclick.server.data.entities.Employee;
 import softclick.server.data.entities.Team;
 import softclick.server.webtier.services.IBaseService;
 
 public interface ITeamService extends IBaseService<Team, Long> {
+    public void addEmployeeToTeam(Long teamId,String EmployeeLastName);
+    void UpdateTeam(Long id, Team newTeam);
 }
