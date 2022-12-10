c2ddf7c40ec4b06cef0c1a688d07c1f0f023f31e
wafaeabo <wafae.abouzbiba@uit.ac.ma>
2022-12-10 22:04:42 +0000

    documenting api

--- src/main/java/softclick/server/webtier/WebTierApplication.java
+++ src/main/java/softclick/server/webtier/WebTierApplication.java
@@ -1,5 +1,6 @@
 package softclick.server.webtier;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -27,6 +28,7 @@ import softclick.server.webtier.services.user.IUserService;
     DataSourceTransactionManagerAutoConfiguration.class,
     HibernateJpaAutoConfiguration.class
 })
+@OpenAPIDefinition
 public class WebTierApplication {
 
     public static void main(String[] args) {
