e7d085dca7e6356862faec0102d40a93740699ac
othmane <othmane.hanyf@protonmail.com>
2022-12-28 22:25:53 +0100

    fixing datetime formatting error

---
+++ src/main/java/softclick/server/webtier/services/role/IRoleService.java
@@ -0,0 +1,7 @@
+package softclick.server.webtier.services.role;
+
+import softclick.server.data.entities.Role;
+import softclick.server.webtier.services.IBaseService;
+
+public interface IRoleService extends IBaseService<Role, Long> {
+}
