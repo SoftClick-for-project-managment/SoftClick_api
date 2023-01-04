155dda8fb7e90ca97d4a8b5444b1473072513b40
Hajar <hajarfz99@gmail.com>
2023-01-04 21:25:09 +0100

    Add Skills Attribute to Employee Entity

--- src/main/java/softclick/server/data/entities/Employee.java
+++ src/main/java/softclick/server/data/entities/Employee.java
@@ -13,7 +13,7 @@ import java.util.Set;
 
 @Entity
 @NoArgsConstructor
-@Data
+//@Data
 @Proxy(lazy=false)
 public class Employee implements Serializable, Comparable<Employee> {
 
@@ -41,6 +41,13 @@ public class Employee implements Serializable, Comparable<Employee> {
             inverseJoinColumns = @JoinColumn( name = "idSkill" ) )
     private Set<Skill> skills;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "Team_Employee",
+            joinColumns = @JoinColumn( name = "idEmployee" ),
+            inverseJoinColumns = @JoinColumn( name = "idTeam" ))
+    private Set<Team> teams;
+
+
     public Employee(String employeeImage, String employeeFirstName, String employeeLastName, String employeeFunction, String employeeEmail, String employeePhone) {
         this.employeeImage = employeeImage;
         this.employeeFirstName = employeeFirstName;
@@ -61,11 +68,77 @@ public class Employee implements Serializable, Comparable<Employee> {
                 .compare(this, employee);
     }
 
+    public Long getId() {
+        return id;
+    }
 
-    @JsonIgnoreProperties("employees")
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployeeImage() {
+        return employeeImage;
+    }
+
+    public void setEmployeeImage(String employeeImage) {
+        this.employeeImage = employeeImage;
+    }
+
+    public String getEmployeeFirstName() {
+        return employeeFirstName;
+    }
+
+    public void setEmployeeFirstName(String employeeFirstName) {
+        this.employeeFirstName = employeeFirstName;
+    }
+
+    public String getEmployeeLastName() {
+        return employeeLastName;
+    }
+
+    public void setEmployeeLastName(String employeeLastName) {
+        this.employeeLastName = employeeLastName;
+    }
+
+    public String getEmployeeFunction() {
+        return employeeFunction;
+    }
+
+    public void setEmployeeFunction(String employeeFunction) {
+        this.employeeFunction = employeeFunction;
+    }
+
+    public String getEmployeeEmail() {
+        return employeeEmail;
+    }
+
+    public void setEmployeeEmail(String employeeEmail) {
+        this.employeeEmail = employeeEmail;
+    }
+
+    public String getEmployeePhone() {
+        return employeePhone;
+    }
+
+    public void setEmployeePhone(String employeePhone) {
+        this.employeePhone = employeePhone;
+    }
+
+    public void setSkills(Set<Skill> skills) {
+        this.skills = skills;
+    }
+
+    public void setTeams(Set<Team> teams) {
+        this.teams = teams;
+    }
+
     public Set<Skill> getSkills() {
         return skills;
     }
 
+    @JsonIgnoreProperties("members")
+    public Set<Team> getTeams() {
+        return teams;
+    }
 
 }
